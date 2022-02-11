7f147c3257b3bf8d7306580008f9ea081ba05e10
Rajat Thakur <rajat@techindustan.com>
2022-02-11 21:15:58 +0530

    eslint setup with husky

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
-import Head from 'next/head'
-
 export default function Home() {
-  return (
-    <div className="bg-teal-400">
-     nextjs tailwind
-    </div>
-  )
+	return <div className="bg-teal-400">nextjs tailwind</div>;
 }
